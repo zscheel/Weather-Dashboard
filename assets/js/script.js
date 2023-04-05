2f6368f922dc4fdbf93b004d19607c82e7c91078
zscheel <zachary.scheel1@gmail.com>
2023-04-04 19:11:05 -0500

    added sumbit function and storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,31 @@ var frm = $("#city-form");
 var historyCards = $("#history");
 var historyCardsTwo = document.querySelector("#history");
 var cityHistory = [];
+
+
+
+
+function addToStorage(parent) {
+    for (i = 0; i < cityHistory.length; i ++) {
+        var city = parent;
+        if(city === cityHistory[i]) {
+            return false;
+        }
+        
+    }
+    var btn = $("<button class='btn btn-secondary m-2'>" + parent + "</button>");
+    historyCards.append(btn);
+    cityHistory.unshift(parent);
+    localStorage.setItem("cityStorage", JSON.stringify(cityHistory));
+}
+
+
+
+
+frm.submit(function(e) {
+    e.preventDefault();
+    var cityName = document.querySelector("#city-search").value;
+    createData(cityName);
+    var frm = document.querySelector("#city-form")
+    frm.reset();
+});
