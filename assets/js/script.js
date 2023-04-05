c38bb547c0058bf0f178b4d594719b7352a18341
zscheel <zachary.scheel1@gmail.com>
2023-04-04 19:09:25 -0500

    added variables to javascript

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var apiKey = "70539c089a192ad51dae93112ad8a637";
+var latLon = [1, 2];
+var weatherDisplay = $("#weather-display");
+var frm = $("#city-form");
+var historyCards = $("#history");
+var historyCardsTwo = document.querySelector("#history");
+var cityHistory = [];
