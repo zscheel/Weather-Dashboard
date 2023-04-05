8ec12863356651be86bab8af762edd6dd3a53151
zscheel <zachary.scheel1@gmail.com>
2023-04-04 19:14:37 -0500

    added functions 1 to grab future weather and other to create data based off cityadded functions 1 to grab future weather and other to create data based off city

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,33 @@ var historyCards = $("#history");
 var historyCardsTwo = document.querySelector("#history");
 var cityHistory = [];
 
+function createData(city) {
+    weatherDisplay.empty();
+    var apiUrlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+    fetch(apiUrlCurrent).then(function(response){
+    if (response.ok){
+        response.json().then(function(data){
+            console.log(data);
+            createCurrentWeather(data);
+            futureWeather(data.coord.lat, data.coord.lon)
+            })
+        } else {
+            alert("Please enter a real city");
+        }  
+    })
+}
 
+function futureWeather(lat, lon) {
+    var apiUrlFuture = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely_hourly_alerts&units=imperial&appid=" + apiKey;
+    fetch(apiUrlFuture).then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                console.log(data);
+                futureCards(data);
+            })
+        }
+    })
+}
 
 
 function addToStorage(parent) {
